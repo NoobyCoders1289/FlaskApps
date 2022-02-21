4cd715a6bbc496d7418b05cb4ee54302585a988e
Chinna Babu <noobycoder6921@gmail.com>
2022-02-21 18:41:48 +0530

    created two routes

--- BlogApp/app.py
+++ BlogApp/app.py
@@ -4,12 +4,15 @@ app=Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello():
-    return "<h1>Hello!!!</h1>"
+    return "<h1>Hello World!!!</h1>"
+
+@app.route('/about')
+def about():
+     return "<h1>About Page</h1>"
 
 
-
-
-
+#To run the app.py in debug Mode
 if __name__ =="__main__":
     app.run(debug=True)
