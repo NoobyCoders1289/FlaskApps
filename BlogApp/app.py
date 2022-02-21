2a6cfe5adfd1a01f3dd0e99992795d4bf47be509
Chinna Babu <noobycoder6921@gmail.com>
2022-02-21 19:01:50 +0530

    working with templates

--- BlogApp/app.py
+++ BlogApp/app.py
@@ -1,17 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
+
+posts = [
+    {
+        'author':'Charan Cherry',
+        'title':'Blog Post 1',
+        'content':'Fisrt post on my website',
+        'date_posted': 'April 20, 2021'
+    },
+    {
+        'author':'Ramya Hasini',
+        'title':'Blog Post 2',
+        'content':'Second post on my website',
+        'date_posted': 'April 22, 2021'
+    },
+]
+
+
 
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return "<h1>Hello World!!!</h1>"
+    return render_template('home.html',posts=posts,title='All Posts')
 
 @app.route('/about')
 def about():
-     return "<h1>About Page</h1>"
-
+     return render_template('about.html')
 
 #To run the app.py in debug Mode
 if __name__ =="__main__":
