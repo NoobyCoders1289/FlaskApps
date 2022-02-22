102c091565335fc185a2729a181d87303e83f980
Chinna Babu <noobycoder6921@gmail.com>
2022-02-22 22:29:31 +0530

    db

--- BlogApp/app.py
+++ BlogApp/app.py
@@ -1,12 +1,43 @@
 from distutils.log import debug
 from turtle import home
 from flask import Flask, redirect, render_template, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 
 from forms import RegistrationForm, LoginForm
 
-app=Flask(__name__)
+import psycopg2
+conn_string = "host='localhost' dbname='blogappdb' user='postgres' password='123123123'"
+conn = psycopg2.connect(conn_string)
 
+
+
+app=Flask(__name__)
 app.config['SECRET_KEY']='673b02697ceecbac65a9591ffeb47c0d'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# db = SQLAlchemy(app)
+
+#sqlalchemy database for users
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+#     password = db.Column(db.String(60), nullable=False)
+#     posts = db.relationship('Post', backref='author', lazy=True)
+
+#     def __repr__(self):
+#         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+#sqlalchemy database for posts
+# class Post(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100), nullable=False)
+#     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#     content = db.Column(db.Text, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+#     def __repr__(self):
+#         return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
